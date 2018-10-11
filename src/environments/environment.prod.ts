1a569d50caac3b21534558b19f7e2bbc7db57d27
Martin Fontan <fontan.martin.cz@gmail.com>
2018-10-11 15:32:13 +0200

    service to ssl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // apiEndpoint: 'https://hamplifikator-server.appspot.com/create_tweeet',
-  apiEndpoint: 'http://31.222.185.153:8080/create_tweeet',
+  //apiEndpoint: 'http://31.222.185.153:8080/create_tweeet',
+  apiEndpoint: 'https://ringpowersolutions.com:8081/create_tweeet',
 };
