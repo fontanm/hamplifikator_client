3ea62482e951114c512dbfdb3420956864857dee
Martin Fontan <fontan.martin.cz@gmail.com>
2020-02-05 15:53:19 +0100

    lib and link update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  //apiEndpoint: 'http://31.222.185.153:8080/create_tweeet',
-  apiEndpoint: 'https://ringpowersolutions.com:8081/create_tweeet',
+  apiEndpoint: 'https://altocumulus.ringpowersolutions.com:8081/create_tweeet',
 };
